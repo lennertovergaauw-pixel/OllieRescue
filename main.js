938fb0e64eaf7008c747344ae24f46a3c7ee0a37
lennertovergaauw-pixel <lennert.overgaauw@gmail.com>
2026-06-17 00:05:31 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const hamburger = document.getElementById('hamburger');
+const mobileMenu = document.getElementById('mobileMenu');
+
+hamburger.addEventListener('click', () => {
+  mobileMenu.classList.toggle('open');
+});
+
+mobileMenu.querySelectorAll('a').forEach(link => {
+  link.addEventListener('click', () => mobileMenu.classList.remove('open'));
+});
